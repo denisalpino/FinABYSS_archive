b384d2c665fc817020331817d736064cfeddb736
denisalpino <denis.tomin.alpino@gmail.com>
2025-03-26 00:03:10 +0300

    Fixed bugs and refined all proxy session management

--- utils/proxy_manager.py
+++ utils/proxy_manager.py
@@ -16,7 +16,7 @@ class ProxyDense:
     ip_adress: str
     port: str
     session: Optional[ClientSession] = field(default=None)
-    max_requests: Semaphore = field(default=Semaphore(14))
+    max_requests: Semaphore = field(default_factory=lambda: Semaphore(14))
 
     def __repr__(self) -> str:
         """
@@ -42,7 +42,7 @@ class ProxyManager:
         self.proxies: List[ProxyDense] = []
         self.__index: int = 0
 
-    def _is_suitable(self, proxy: ProxyDense) -> bool:
+    def _is_suitable(self, proxy: ProxyDense) -> bool | Exception:
         """Verify whether proxy can connect to the testing url"""
         try:
             resp = requests.get(
@@ -50,8 +50,8 @@ class ProxyManager:
                 proxies={proxy.protocol: str(proxy)}
             )
             assert proxy.ip_adress == resp.json()["origin"]
-        except Exception:
-            return False
+        except Exception as error:
+            return error
         return True
 
     def add_proxy(
@@ -68,12 +68,13 @@ class ProxyManager:
         elif not proxy:
             proxy = ProxyDense(protocol, login, password, ip_adress, port)
 
-        if self._is_suitable(proxy):
+        test = self._is_suitable(proxy)
+
+        if test == True:
             self.proxies.append(proxy)
         else:
             raise ConnectionError(
-                f"Bad proxy {proxy.ip_adress}. "
-                "Haven't connected to https://httpbin.io/ip."
+                f"Bad proxy {proxy.ip_adress}. Error: {test}"
             )
 
     def _switch_proxy(self) -> None:
